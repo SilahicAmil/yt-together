aa3cb03d9a532ad6c5284846eceb5592c137297d
Amil Silahic <jinobie02@gmail.com>
2025-07-14 20:36:59 -0600

    Updating some styling for the room page. Making it slightly responsive for desktop and mobile. Extracting these components out l8r

--- frontend/src/room/room-details.jsx
+++ frontend/src/room/room-details.jsx
@@ -4,26 +4,26 @@ export default function RoomDetails() {
       <div className="flex justify-center items-center">
         <h1 className="text-4xl">ROOM NAME</h1>
       </div>
-      <div className="m-5 float-left w-[60%]">
+      <div className="m-5 lg:float-left lg:w-[60%]">
         <div className="mb-4 flex gap-24">
           <input
             type="search"
             defaultValue="YOUTUBE VIDEO LINK"
-            className="bg-amber-300 w-1/3 rounded-xl pl-4"
+            className="bg-amber-300 lg:w-1/3 rounded-xl pl-4"
           />
 
-          <button className="bg-blue-300 p-4 rounded-xl w-32">Enter</button>
+          <button className="bg-blue-300 p-4 rounded-xl lg:w-32">Enter</button>
         </div>
         <h1 className="text-2xl font-mono">YOUTUBE VIDEO TITLE</h1>
         <div className="h-125 w-full mt-5 bg-green-300 flex items-center justify-center">
           YOUTUBE VIDEO
         </div>
       </div>
-      <div className="float-right w-[30%] mr-8 h-full mt-5">
-        <div className="h-68 w-full bg-red-500">
+      <div className="lg:float-right lg:w-[30%] h-full m-5">
+        <div className="h-64 w-full bg-red-500">
           <p>User 1 has joined the room!</p>
         </div>
-        <div className="h-64 w-full bg-pink-300 mt-24">
+        <div className="h-64 w-full bg-pink-300 mt-4">
           <p>WATCH HISTORY</p>
         </div>
       </div>
