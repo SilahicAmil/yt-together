bf3edc04467677888a1a3f7d9daa0226db51537a
Amil Silahic <jinobie02@gmail.com>
2025-07-23 17:06:52 -0600

    Working on getting youtube video embeds working. So far so good, just a really simple site

--- frontend/src/room/room-details.jsx
+++ frontend/src/room/room-details.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useWebSocket from "react-use-websocket";
 
 export default function RoomDetails() {
+  const [youtubeLink, setYoutubeLink] = useState("");
   let { id } = useParams();
 
   const socketUrl = `ws://localhost:8080/ws/${id}`; // Use ws:// when doing ws
@@ -15,7 +16,18 @@ export default function RoomDetails() {
     }
   }, [lastMessage]);
 
-  function handlePlay() {
+  function handleYoutubeLink(e) {
+    e.preventDefault();
+    var videoLink = e.target.value;
+    if (videoLink.includes("/watch?v=")) {
+      console.log("contains");
+      videoLink = videoLink.replace("watch?v=", "embed/");
+    }
+    setYoutubeLink(videoLink);
+    // Push to some type of history
+  }
+
+  function handlePlay(e) {
     sendJsonMessage({ action: "play" });
   }
 
@@ -25,28 +37,20 @@ export default function RoomDetails() {
     // User and Watch History will be some type of reusable like card component
     //
     <>
-      <button onClick={handlePlay}>Handle Play</button>
       <div className="flex justify-center items-center">
         <h1 className="text-4xl">ROOM NAME</h1>
       </div>
       <div className="m-5 lg:float-left lg:w-[60%]">
-        <div className="mb-4 flex gap-24">
+        <div className="">
           <input
             type="search"
-            defaultValue="YOUTUBE VIDEO LINK"
-            className="bg-amber-300 lg:w-1/3 rounded-xl pl-4"
+            value={youtubeLink}
+            onChange={handleYoutubeLink}
+            className="bg-amber-300 lg:w-1/3 rounded-xl pl-4 w-full h-16"
           />
-
-          <button
-            className="bg-blue-300 p-4 rounded-xl lg:w-32"
-            onClick={handlePlay}
-          >
-            Enter
-          </button>
         </div>
-        <h1 className="text-2xl font-mono">YOUTUBE VIDEO TITLE</h1>
-        <div className="h-125 w-full mt-5 bg-green-300 flex items-center justify-center">
-          YOUTUBE VIDEO
+        <div className="h-125 w-full mt-5 flex items-center justify-center">
+          <iframe src={youtubeLink} className="w-full h-full"></iframe>
         </div>
       </div>
       <div className="lg:float-right lg:w-[30%] h-full m-5">
